1871bfb97f06ab66765c43626579911e1910c19f
Chaz Simons <chaz.simons@gmail.com>
2021-09-27 19:37:39 -0700

    Add tests and methods for night_reader class

--- spec/night_reader_spec.rb
+++ spec/night_reader_spec.rb
@@ -12,4 +12,18 @@ describe NightReader do
     expect(@reader).to be_an_instance_of(NightReader)
   end
 
+  it 'can read a file' do
+    results = @reader.read('braille.txt')
+    expect(results).to be_a(String)
+  end
+
+  it 'can create a new instance of translator' do
+    results = @reader.translate('braille.txt', 'message.txt')
+    expect(results).to be_an_instance_of(Translator)
+  end
+
+  it 'can create a message' do
+    results = @reader.creation_message('braille.txt', 'message.txt')
+    expect(results).to eq("Created 'message.txt' containing 11 characters")
+  end
 end
