812ca319d0aa2f428d373e573060ba80a556afea
Chaz Simons <chaz.simons@gmail.com>
2021-09-27 18:21:26 -0700

    Add test and method to breakdown the message

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,14 +1,15 @@
 require_relative 'dictionary'
 class Translator
   include Dictionary
-
+  attr_reader :message,
+              :path
   def initialize(in_file, out_file)
     @message = in_file
     @path    = out_file
   end
 
   def breakdown(message)
-    characters = message.chomp.split(//)
+    characters = message.split(//)
     characters
   end
 end
--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -12,4 +12,15 @@ describe Translator do
   it 'exists' do
     expect(@translator).to be_an_instance_of(Translator)
   end
+
+  it 'has attributes' do
+    expect(@translator.message).to eq('message.txt')
+    expect(@translator.path).to eq('braille.txt')
+  end
+
+  it 'can break a message into an array' do
+    results = @translator.breakdown('this is a string')
+    expect(results.count).to eq(16)
+    expect(results).to be_an(Array)
+  end
 end
